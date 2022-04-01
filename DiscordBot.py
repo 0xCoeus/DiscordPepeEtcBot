3472842da26d6588872ba0d0be7b851114006c90
0xCoeus <nkmanbusiness@gmail.com>
2022-04-01 07:31:29 -0400

    update

--- DiscordBot.py
+++ DiscordBot.py
@@ -2,29 +2,28 @@ import os
 import discord
 import random
 from dotenv.main import load_dotenv
+from discord.ext import commands
 
 
 def main():
 
-    client = discord.Client() 
+    bot = commands.Bot(command_prefix = '$')
     load_dotenv()
 
-    @client.event
+    @bot.event
     async def on_ready():
         token = os.getenv("DISCORD_TOKEN_ID")
-        print('We have logged in as {0.user}'.format(client))
+        print('We have logged in as {0.user}'.format(bot))
 
-    @client.event
-    async def on_message(message):
-        token = os.getenv("DISCORD_TOKEN_ID")
-        if message.content == '$pepe':
-          path = "C:/Users/nkman/Crypto_Twitter/pepes"
-          files = os.listdir(path)
-          d = random.choice(files)
-          await message.channel.send(file=discord.File(f"C:/Users/nkman/Crypto_Twitter/pepes/{d}"))
+    @bot.command() 
+    async def pepe(ctx):
+        path = "C:/Users/nkman/Crypto_Twitter/pepes"
+        files = os.listdir(path)
+        d = random.choice(files)
+        await ctx.channel.send(file=discord.File(f"C:/Users/nkman/Crypto_Twitter/pepes/{d}"))
 
 
-    client.run(os.getenv("DISCORD_TOKEN_ID"))
+    bot.run(os.getenv("DISCORD_TOKEN_ID"))
 
 if __name__ == '__main__':
     main()
